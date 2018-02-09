d7a090a0f923ba938cf9957accc2fde9bbb5771b
sjtg <chipilej@gmail.com>
2018-02-09 12:22:12 +0200

    Created a StudentSignView

--- school_dashboard/school_app/views.py
+++ school_dashboard/school_app/views.py
@@ -3,4 +3,28 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.contrib.auth import login
+
+from django.shortcuts import redirect
+
+from django.views.generic import CreateView
+
+from .forms import StudentSignUpForm
+from . models import User
+
 # Create your views here.
+
+
+class StudentSignUpView(CreateView):
+	model = User
+	form_class = StudentSignUpForm
+	template_name = 'registration/signup_form.html'
+
+	def get_context_data(self, **kwargs):
+		kwargs['user_type'] = 'student'
+		return super().get_context_data(**kwargs)
+
+	def form_valid(self, form):
+		user = form.save()
+		login(self.request, user)
+		return redirect('students:quiz_list')
